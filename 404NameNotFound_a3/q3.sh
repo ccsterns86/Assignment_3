77b8573bcaf0e733bf85d233b9d2f1c21079210d
ccsterns86 <cast2053@colorado.edu>
2023-03-01 12:20:45 +0000

    fixed q3

--- 404NameNotFound_a3/q3.sh
+++ 404NameNotFound_a3/q3.sh
@@ -2,11 +2,11 @@
 
 #Cassandra Sterns      SID: 22209739
 #Ethan Doyle           SID: 22210635
-
-while read line
-do
-  read -a myvar <<< $line
-  for var in "${myvar[@]:1}"
-    do echo "$var"
-  done
-done
+cat sample.txt | cut -d " " -f 2-16 | tr ' ' '\n'
+# while read line
+# do
+#   read -a myvar <<< $line
+#   for var in "${myvar[@]:1}"
+#     do echo "$var"
+#   done
+# done
